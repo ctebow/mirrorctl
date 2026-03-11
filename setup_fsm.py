4e72589ae518653db866c36fcf724d365d21bae9
Caden Tebow <cadentebow@Cadens-MacBook-Air.local>
2026-03-11 13:02:32 -0500

    Fix gpio numbering

--- setup_fsm.py
+++ setup_fsm.py
@@ -20,8 +20,8 @@ import time
 import voltage_helpers as helpers
 
 # filter clock pins
-FCLK_PWM_PIN_1 = 4  # Must be a PWM-capable pin; config-pin to pwm if needed
-FCLK_PWM_PIN_2 = 5
+FCLK_PWM_PIN_1 = 12  # Must be a PWM-capable pin; config-pin to pwm if needed
+FCLK_PWM_PIN_2 = 13
 FCLK_HZ = 30_000
 FCLK_DUTY_PERCENT = 500000
 
